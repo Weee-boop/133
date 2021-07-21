3d68781bdccc86ca76f3edc3f07679afb5251e0f
Weee-boop <79670593+Weee-boop@users.noreply.github.com>
2021-07-21 13:43:27 -0500

    Add files via upload

---
+++ bed - Copy - Copy (4).js
@@ -0,0 +1,50 @@
+cats_dog="";
+object_detect=[];
+status="";
+function preload(){
+    cats_dog=loadImage("Home_office_small_office.jpg");
+}
+
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector= ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log ("cocossd is Initialized");
+    status=true;
+    objectDetector.detect(cats_dog,gotResult);
+}
+
+function gotResult(error,result){
+    if(error){
+        console.log (error);
+    }
+    else{
+        console.log (result);
+        objects_detect=result;
+    }
+}
+
+function draw(){
+    image(cats_dog,0,0,640,420);
+    
+    if(status != ""){
+        for(i=0 ; i<object_detect.length ; i++){
+            document.getElementById("status").innerHTML="Status: Object detected";
+            document.getElementById("things").innerHTML="COCOSSD identify " + object_detect.length + " objects";
+            percent=floor(object_detect[i].confidence*100);
+            fill("#000000");
+            text(object_detect[i].label+" "+percent+"%",object_detect[i].x+15,object_detect[i].y+15);
+            noFill();
+            stroke("#000000");
+            rect(object_detect[i].x,object_detect[i].y,object_detect[i].width,object_detect[i].height);
+        }
+    }
+}
+
+function back(){
+    window.location="index.html";
+}
